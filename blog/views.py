feab30f44e8a448812023e7d30109c20c2652be7
Yisselda <yisselda@defmethod.io>
2017-10-26 13:46:48 -0400

    redirect unidentified users

--- blog/views.py
+++ blog/views.py
@@ -17,14 +17,35 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if request.user.is_authenticated():
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_new.html', {'form': form})
     else:
-        form = PostForm()
-    return render(request, 'blog/post_new.html', {'form': form})
+        return redirect('post_list')
+
+
+def post_edit(request, pk):
+    if request.user.is_authenticated():
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_new.html', {'form': form})
+    else:
+        return redirect('post_list')
